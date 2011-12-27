1a2d88dba250397fbad6b268f3a2d99ff73b4aa8
Anderson Lin <d.trisix@gmail.com>
2011-12-27 12:59:58 +0800

    Fix FBDialog error by ignore NSURLErrorDomain code -999 http://bit.ly/v854E5

--- Classes/ShareKit/Sharers/Services/Facebook/FBConnect/FBDialog.m
+++ Classes/ShareKit/Sharers/Services/Facebook/FBConnect/FBDialog.m
@@ -409,10 +409,10 @@ params   = _params;
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
-    // 102 == WebKitErrorFrameLoadInterruptedByPolicyChange
-    if (!([error.domain isEqualToString:@"WebKitErrorDomain"] && error.code == 102)) {
-        [self dismissWithError:error animated:YES];
-    }
+  // 102 == WebKitErrorFrameLoadInterruptedByPolicyChange
+  if (!(([error.domain isEqualToString:@"WebKitErrorDomain"] && error.code == 102)||([error.domain isEqualToString:@"NSURLErrorDomain"] && error.code == -999))) {
+    [self dismissWithError:error animated:YES];
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
